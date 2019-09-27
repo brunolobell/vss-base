5bfc4c03a53b01baf2c469cb35371d9343a5957e
Miguel Martins dos Santos <miguelmartinsdossantos@gmail.com>
2019-09-27 17:11:12 -0300

    Serial Sender Imported

---
+++ include/io/serial_message.h
@@ -0,0 +1,56 @@
+// Copyright FURGBot 2019
+
+
+#ifndef SERIAL_MESSAGE_H
+#define SERIAL_MESSAGE_H
+
+
+#include <iostream>
+#include <stdint.h>
+#include <vector>
+#include <string>
+
+
+namespace vss_furgbol {
+namespace io {
+
+class SerialMessage {
+    private:
+        uint16_t pkg_id_;
+        uint8_t msg_type_;
+        uint8_t robot_id_;
+        uint8_t velocity_linear_;
+        uint8_t velocity_angular_;
+        uint8_t direction_linear_;
+        uint8_t direction_angular_;
+        
+    public:
+        SerialMessage();
+        ~SerialMessage();
+
+        void serialize(std::vector<unsigned char> &buffer);
+
+        void clear();
+
+        void setPkgId(uint16_t pkg_id);
+        void setMsgType(uint8_t msg_type);
+        void setRobotId(uint8_t robot_id);
+        void setVelocityLinear(uint8_t velocity_linear);
+        void setVelocityAngular(uint8_t velocity_angular);
+        void setDirectionLinear(uint8_t direction_linear);
+        void setDirectionAngular(uint8_t direction_angular);
+
+        uint16_t getPkgId();
+        uint8_t getMsgType();
+        uint8_t getRobotId();
+        uint8_t getVelocityLinear();
+        uint8_t getVelocityAngular();
+        uint8_t getDirectionLinear();
+        uint8_t getDirectionAngular();
+};
+
+}  // namespace io
+}  // namespace vss_furgbol
+
+
+#endif // SERIAL_MESSAGE_H
---
+++ src/io/serial_message.cc
@@ -0,0 +1,51 @@
+// Copyright FURGBot 2019
+
+#include "io/serial_message.h"
+
+namespace vss_furgbol {
+namespace io {
+
+SerialMessage::SerialMessage() { clear(); }
+
+SerialMessage::~SerialMessage() {}
+
+void SerialMessage::clear() {
+    pkg_id_ = 0;
+    msg_type_ = 0;
+    robot_id_ = 0;
+    velocity_linear_ = 0;
+    velocity_angular_ = 0;
+    direction_linear_ = 0;
+    direction_angular_ = 0;
+}
+
+void SerialMessage::setPkgId(uint16_t pkg_id) { pkg_id_ = pkg_id; }
+
+void SerialMessage::setMsgType(uint8_t msg_type) { msg_type_ = msg_type; }
+
+void SerialMessage::setRobotId(uint8_t robot_id) { robot_id_ = robot_id; }
+
+void SerialMessage::setVelocityLinear(uint8_t velocity_linear) { velocity_linear_ = velocity_linear; }
+
+void SerialMessage::setVelocityAngular(uint8_t velocity_angular) { velocity_angular_ = velocity_angular; }
+
+void SerialMessage::setDirectionLinear(uint8_t direction_linear) { direction_linear_ = direction_linear; }
+
+void SerialMessage::setDirectionAngular(uint8_t direction_angular) { direction_angular_ = direction_angular; }
+
+uint16_t SerialMessage::getPkgId() { return pkg_id_; }
+
+uint8_t SerialMessage::getMsgType() { return msg_type_; }
+
+uint8_t SerialMessage::getRobotId() { return robot_id_; }
+
+uint8_t SerialMessage::getVelocityLinear() { return velocity_linear_; }
+
+uint8_t SerialMessage::getVelocityAngular() { return velocity_angular_; }
+
+uint8_t SerialMessage::getDirectionLinear() { return direction_linear_; }
+
+uint8_t SerialMessage::getDirectionAngular() { return direction_angular_; }
+
+} // namespace io
+} // namespace vss_furgbol
